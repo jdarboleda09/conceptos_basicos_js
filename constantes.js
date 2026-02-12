3a04986d468ae0a0fcbee96769d3e803169104fb
jdarboleda09 <jd.arboleda.g@gmail.com>
2026-02-11 19:20:03 -0500

    este es un taller en clase de JavaSacript Basico

---
+++ constantes.js
@@ -0,0 +1,4 @@
+const nombre = "Juan David Arboleda Grisales"
+const edad = 32
+
+console.log("Hola, mi nombre es" , nombre , "y tengo" , edad , "años")
---
+++ variables.js
@@ -0,0 +1,7 @@
+let nombre =  "Juan"
+let apellido =  "Arboleda"
+let grupo = 1154
+let curso = "JavaScript Basico"
+let clase = "Definición de Variables"
+
+console.log("Soy",nombre,apellido, "estoy en el grupo", grupo , "estamos en curso de" , curso, "viendo clase de" , clase)
